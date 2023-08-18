bf6c422e586387bd2a62dfd84352cb4eb556674c
rihhana <hrihhana@gmail.com>
2023-08-18 20:13:24 +0300

    answers of first-Names

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
 // Good Luck 💪🏾
+ const gitFirstNames=([[ 'ahmed','aisha','Amina' ],["Jaamac","Khadijah","Layla"],["Mohamed","Ali"]])
+  console.log(gitFirstNames[0][0],gitFirstNames[1][0],gitFirstNames[2][0])
